d743306681eadb7c269a7313e3fc2ab5fceace0f
Ashis <developer.ashis99@gmail.com>
2023-07-13 23:11:35 +0530

    Fixing Bugs

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-mongoose.connect(process.env.mongooseUrl);
+mongoose.connect(process.env.mongoDBAtlas);
 
 const db = mongoose.connection;
 
